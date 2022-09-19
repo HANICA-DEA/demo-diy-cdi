df76d19a7d63f1a61844eb5dc2e28fbedbfcf3c5
Rody Middelkoop <rody.middelkoop@gmail.com>
2022-09-19 15:58:38 +0200

    - Changed unit test to use Mockito - Added unit test to use @Mock & @InjectMock

--- src/test/java/nl/han/aim/oose/dea/resources/ItemResourceAnnotatedTest.java
+++ src/test/java/nl/han/aim/oose/dea/resources/ItemResourceAnnotatedTest.java
@@ -3,21 +3,29 @@ package nl.han.aim.oose.dea.resources;
 import nl.han.aim.oose.dea.services.ItemService;
 import nl.han.aim.oose.dea.services.dto.ItemDTO;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class ItemResourceTest {
+@ExtendWith(MockitoExtension.class)
+class ItemResourceAnnotatedTest {
+    @InjectMocks
+    ItemResource itemResource;
+
+    @Mock
+    ItemService itemService;
 
     @Test
     void getJsonItems_whenCalled_ShouldReturnThreeItems() {
         // Arrange
-        ItemResource itemResource = new ItemResource();
-        ItemService itemService = mock(ItemService.class);
         List<ItemDTO> products = new ArrayList<>();
         products.add(new ItemDTO(0, "", new String[]{}, ""));
         products.add(new ItemDTO(1, "", new String[]{}, ""));
